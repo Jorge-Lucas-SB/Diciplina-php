bf7565f28ec66aa95b75d4028927464ac9c219c2
jorge-lsb <jorge.barbosa@bandtec.com.br>
2020-05-27 23:15:59 -0300

    Inicialização do projeto php

---
+++ pages/formulario.php
@@ -0,0 +1,52 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title> Formulário HTML </title>
+
+    <style>
+
+    </style>
+
+    <link rel="stylesheet" href="../css/style_formulario.css">
+</head>
+
+<body>
+    <div class='formulario'>
+
+        <form action="enviar.php" method="post">
+            
+                <h4>Formulario de mensagem</h4>
+
+                <label for="nome">Nome:</label>
+
+                <input type="text" name="nome" id="nome" required> <br>
+
+                <label for="idade">Idade:</label>
+
+                <input type="number" name="idade" id="idade" required> <br>
+
+                <label for="site">Site: </label>
+
+                <input type="url" name="site" id="site" required> <br>
+
+                <label for="email">Email: </label>
+
+                <input type="email" name="email" id="email" required> <br>
+
+                <label for="mensagem">Mensagem: </label>
+
+                <textarea name="mensagem" id="mensagem" required></textarea> <br> <br>
+
+                <input type="submit" value="Enviar">
+
+        </form>
+    </div>
+
+</body>
+
+</html>
